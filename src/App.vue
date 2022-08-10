447c0f35748d896418ee7be0d8b625639096a3e5
Tran-Thuong Tien-Thinh <tranthuongtienthinh@gmail.com>
2022-08-10 19:39:36 +0700

    vue

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,76 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <canvas id="canvas" width="500" height="500" @mousemove="mousemove" @mousedown="mouseevent(true)" @mouseup="mouseevent(false)"></canvas>
+  <div>
+    <h1>Choose color : {{ color }}</h1>
+
+    <input type="radio" id="black" value="Black" v-model="color">
+    <label for="black">Black</label>
+
+    <input type="radio" id="white" value="White" v-model="color">
+    <label for="white">White</label>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      width: 500,
+      height: 500,
+      color: 'black',
+      canvas: null,
+      ctx: null,
+      mouse: {
+        previous: {
+          x: 0,
+          y: 0
+        },
+        down: false
+      }
+    }
+  },
+  mounted() {
+    this.canvas = document.getElementById('canvas'),
+    this.ctx = this.canvas.getContext('2d');
+  },
+  methods: {
+    scalePosition(x, y) {
+        let posX = this.canvas.offsetLeft;
+        let posY = this.canvas.offsetTop;
+        let diffX = this.canvas.offsetWidth;
+        let diffY = this.canvas.offsetHeight;
+        let ratioX = this.width/diffX;
+        let ratioY = this.height/diffY;
+        return {x:(x-posX)*ratioX, y:(y-posY)*ratioY}
+    },
+    mouseevent(down) {
+      this.mouse.down = down;
+    },
+    mousemove(event) {
+      if (this.mouse.down) {
+        
+        this.ctx.beginPath();
+        this.ctx.strokeStyle = this.color;
+        console.log(this.ctx.fillStyle)
+        let result = this.scalePosition(this.mouse.previous.x, this.mouse.previous.y);
+        this.ctx.moveTo(result.x, result.y);
+        result = this.scalePosition(event.clientX, event.clientY);
+        this.ctx.lineTo(result.x, result.y);
+        this.ctx.stroke();
+      }
+      this.mouse.previous.x = event.clientX;
+      this.mouse.previous.y = event.clientY;
+    }
   }
 }
+
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+canvas {
+  width: 500px;
+  height: 500px;
+  background-color: grey;
 }
 </style>
---
+++ src/components/canvasElement.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <p>test : {{message}}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'canvasElement',
+  props: ['message'],
+  
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
